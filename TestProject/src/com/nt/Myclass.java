4a58c522ffbde38e9874864e296814908d7581dd
ganeshdekatc <ganesh.bute@dekatc.com>
2021-02-05 01:47:06 +0530

    Add contructor

--- TestProject/src/com/nt/Myclass.java
+++ TestProject/src/com/nt/Myclass.java
@@ -3,7 +3,14 @@ public class Myclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		 
 
+	}
+
+	public Myclass() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 }
